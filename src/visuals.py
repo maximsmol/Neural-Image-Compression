9f17d4098f5bae1d867be670a01949ab7eaec30c
Maksim Smolin <maximsmol@gmail.com>
2019-12-01 21:38:00 -0800

    Improve training cycle

--- src/visuals.py
+++ src/visuals.py
@@ -1,14 +1,16 @@
 import matplotlib
 import matplotlib.pyplot as plt
 
-def show_side_by_side(a, b):
+def show_side_by_side(l):
   plt.close()
 
-  a = a.permute(1, 2, 0)
-  b = b.permute(1, 2, 0)
+  fig, axes = plt.subplots(len(l), 2, squeeze=False)
+  for i in range(len(l)):
+    a, b = l[i]
+    a = a.permute(1, 2, 0)
+    b = b.permute(1, 2, 0)
 
-  fig, axes = plt.subplots(1, 2)
-  axes[0].imshow(a)
-  axes[1].imshow(b)
+    axes[i][0].imshow(a)
+    axes[i][1].imshow(b)
 
-  plt.show()
+  plt.savefig('side-by-side.pdf')
