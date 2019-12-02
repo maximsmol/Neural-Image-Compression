7a3f5cb96f3935f62e15356231796bfaf175ffd8
Maksim Smolin <1472826+maximsmol@users.noreply.github.com>
2019-12-01 23:22:12 -0800

    Improve checkpointing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import pickle
 import os
 import time
 
@@ -34,15 +35,29 @@ optimiser = optim.Adam(model.parameters())
 model.train()
 train_losses = []
 
+chk_data = {
+  'lastBatch': 0,
+  'lastEpoch': 0
+}
 if args.resume:
   print('Loading from checkpoint')
   model.load_state_dict(torch.load('checkpoint/model.pth'))
   optimiser.load_state_dict(torch.load('checkpoint/optimiser.pth'))
 
+  chk_data = pickle.load(open('checkpoint/data.pickle', 'rb'))
+
+  print(f'Starting from {chk_data["lastEpoch"]}/{chk_data["lastBatch"]}')
+
 # fixme: epochs aren't epochs
 history = []
 for epoch in range(args.epochs):
-  for i, (data, target) in enumerate(train_loader):
+  torch.manual_seed(chk_data['lastEpoch'])
+
+  load_iter = iter(train_loader)
+  for j in range(chk_data['lastBatch']):
+    load_iter.__next__()
+
+  for i, (data, target) in enumerate(load_iter):
     batch_start = time.time()
 
     data = data.to(device=device, non_blocking=True)
@@ -59,19 +74,26 @@ for epoch in range(args.epochs):
     optimiser.step()
 
     batch_end = time.time()
+    chk_data['lastBatch'] += 1
 
     if i % args.save_interval == 0:
       history.append((target[0], output[0].round().detach()))
       history = history[-10:]
       show_side_by_side(history)
 
-      print(f'  Batch {i}: loss={loss.item():.2} ~ {1/(batch_end-batch_start):.2} b/s')
+      print(f'  Batch {chk_data["lastEpoch"]}/{chk_data["lastBatch"]}: loss={loss.item():.2} ~ {1/(batch_end-batch_start):.2} b/s')
+
+      pickle.dump(chk_data, open('checkpoint/data1.pickle', 'wb'))
+      os.replace('checkpoint/data1.pickle', 'checkpoint/data.pickle')
 
       torch.save(model.state_dict(), 'checkpoint/model1.pth')
       os.replace('checkpoint/model1.pth', 'checkpoint/model.pth')
       torch.save(optimiser.state_dict(), 'checkpoint/optimiser1.pth')
       os.replace('checkpoint/optimiser1.pth', 'checkpoint/optimiser.pth')
-  print(f'Epoch {epoch}: avg loss={mean(train_losses):.2}')
+  print(f'Epoch {chk_data["lastEpoch"]}: avg loss={mean(train_losses):.2}')
   train_losses = []
+
+  chk_data['lastEpoch'] += 1
+  chk_data['lastBatch'] = 0
 
 print('Done!')
