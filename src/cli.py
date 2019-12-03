418d64914f0270560ca9a92544e76cef5ea68e9f
Maksim Smolin <1472826+maximsmol@users.noreply.github.com>
2019-12-03 12:54:15 -0800

    Update default intervals

--- src/cli.py
+++ src/cli.py
@@ -5,11 +5,11 @@ parser.add_argument('--epochs', type=int, default=100, metavar='N',
                     help='set the target number of training epochs (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='do not use CUDA even if available')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+parser.add_argument('--log-interval', type=int, default=60, metavar='N',
                     help='number of seconds to wait between logging training progress')
-parser.add_argument('--backup-interval', type=int, default=60*30, metavar='N',
+parser.add_argument('--backup-interval', type=int, default=60*60, metavar='N',
                     help='number of seconds to wait between saving another backup when checkpointing')
-parser.add_argument('--save-interval', type=int, default=60, metavar='N',
+parser.add_argument('--save-interval', type=int, default=60*5, metavar='N',
                     help='number of seconds to wait between checkpoints')
 parser.add_argument('--resume', action='store_true', default=False,
                     help='resume training from the last checkpoint')
