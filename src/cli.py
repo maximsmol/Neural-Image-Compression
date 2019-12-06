ca3bb7656b6a49979841a8197d93f775965f7322
Maksim Smolin <1472826+maximsmol@users.noreply.github.com>
2019-12-05 19:30:40 -0800

    Replace resume option with --restart

--- src/cli.py
+++ src/cli.py
@@ -11,8 +11,8 @@ parser.add_argument('--backup-interval', type=int, default=60*30, metavar='N',
                     help='number of seconds to wait between saving another backup when checkpointing')
 parser.add_argument('--save-interval', type=int, default=60*5, metavar='N',
                     help='number of seconds to wait between checkpoints')
-parser.add_argument('--resume', action='store_true', default=False,
-                    help='resume training from the last checkpoint')
+parser.add_argument('--restart', action='store_true', default=False,
+                    help='always restart training even if there is a checkpoint')
 parser.add_argument('--mode', default='train',
                     help='train/eval/infer')
 args = parser.parse_args()
